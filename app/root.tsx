f8905df09caac3e04e4d18c7a1096a3c9b6a40bc
P3ntest <p3ntest@gmx.de>
2022-05-19 14:48:52 +0200

    minor updates

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ import { useState } from 'react';
 
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'New Remix App',
+    title: 'License Server',
     viewport: 'width=device-width,initial-scale=1',
 });
 
