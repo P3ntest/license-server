7d237f4550cd14d0309167a11dedcae6b7699b13
P3ntest <p3ntest@gmx.de>
2022-05-19 12:55:24 +0200

    progress on license editor

--- app/routes/dashboard/apps/$appId/licenses/$licenseId.tsx
+++ app/routes/dashboard/apps/$appId/licenses/$licenseId.tsx
@@ -12,16 +12,19 @@ import {
     Textarea,
     Container,
     TextInput,
+    Switch,
 } from '@mantine/core';
-import { App, License, LicenseAccess } from '@prisma/client';
-import { LoaderFunction } from '@remix-run/node';
+import type { App, License, LicenseAccess } from '@prisma/client';
+import type { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { Form, Link, useLoaderData } from '@remix-run/react';
-import { useInputState } from '@mantine/hooks';
+import { useBooleanToggle, useInputState, useToggle } from '@mantine/hooks';
 import {
     ArrowBack,
     ArrowLeft,
     ArrowLoopLeft,
+    Box,
     Clock,
+    DatabaseExport,
     ShieldCheck,
 } from 'tabler-icons-react';
 import { Badges } from '~/compontents/dashboard/license';
@@ -104,6 +107,8 @@ const PropEditor = ({ license }: { license: License }) => {
     const [payload, setPayload] = useInputState(license.payload);
     const [label, setLabel] = useInputState(license.label);
 
+    const [ipLimited, setIpLimited] = useBooleanToggle(license.ipLimited);
+
     const [ipInput, setIpInput] = useInputState(license.ips.join('\n'));
     const [blackListInput, setBlackListInput] = useInputState(
         license.ipBlacklist.join('\n')
@@ -116,17 +121,20 @@ const PropEditor = ({ license }: { license: License }) => {
         payload != license.payload ||
         JSON.stringify(ips) != JSON.stringify(license.ips) ||
         JSON.stringify(ipBlacklist) != JSON.stringify(license.ipBlacklist) ||
-        label != license.label;
+        label != license.label ||
+        ipLimited != license.ipLimited;
 
     return (
         <Form method='patch'>
             <Container>
                 <Stack>
+                    <Title order={4}>General</Title>
                     <TextInput
                         value={label}
                         onChange={setLabel}
                         label='License Label'
                         placeholder='My license'
+                        name='label'
                     />
                     <Textarea
                         label='Payload'
@@ -134,6 +142,15 @@ const PropEditor = ({ license }: { license: License }) => {
                         onChange={setPayload}
                         placeholder='Payload'
                         autosize
+                        name='payload'
+                    />
+                    <Title order={4}>IP Access Settings</Title>
+                    <Switch
+                        label='IP Whitelist Enabled'
+                        checked={ipLimited}
+                        onChange={box => setIpLimited(box.currentTarget.checked)}
+                        value='ipLimited'
+                        name='ipLimited'
                     />
                     <Textarea
                         defaultValue={license.ips.join('\n')}
@@ -142,6 +159,7 @@ const PropEditor = ({ license }: { license: License }) => {
                         value={ipInput}
                         onChange={setIpInput}
                         autosize
+                        name='ips'
                     />
                     <Textarea
                         defaultValue={license.ipBlacklist.join('\n')}
@@ -150,6 +168,7 @@ const PropEditor = ({ license }: { license: License }) => {
                         value={blackListInput}
                         onChange={setBlackListInput}
                         autosize
+                        name='ipBlacklist'
                     />
                     <Button type='submit' disabled={!changed}>
                         Save
@@ -183,4 +202,28 @@ export const loader: LoaderFunction = async ({ params, request }) => {
         accesses: license!.accesses,
         app: license!.app,
     } as LoaderData;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+    const account = await getAccount(request);
+    await hasPermission(params.appId!, account.id, true);
+
+    const formData = await request.formData();
+
+    await db.license.update({
+        where: {
+            id: params.licenseId!,
+        },
+        data: {
+            label: formData.get('label') as string,
+            payload: formData.get('payload') as string,
+            ips: (formData.get('ips') as string).split('\n').filter(ip => ip !== ''),
+            ipBlacklist: (formData.get('ipBlacklist') as string)
+                .split('\n')
+                .filter(ip => ip !== ''),
+            ipLimited: formData.has('ipLimited'),
+        },
+    });
+
+    return {};
 };
