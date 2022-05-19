51cfcd3f3f93da7c9a896c4f39a020e8c5dbc3f3
P3ntest <p3ntest@gmx.de>
2022-05-19 10:18:14 +0200

    add api :fire:

--- app/routes/dashboard/apps/$appId/licenses/index.tsx
+++ app/routes/dashboard/apps/$appId/licenses/index.tsx
@@ -1,160 +1,219 @@
-import { Badge, Card, Group, Text, Title, Divider, SimpleGrid, Button, Modal, Indicator, Switch, Stack, JsonInput, Textarea, TextInput } from "@mantine/core";
+import {
+  Badge,
+  Card,
+  Group,
+  Text,
+  Title,
+  Divider,
+  SimpleGrid,
+  Button,
+  Modal,
+  Indicator,
+  Switch,
+  Stack,
+  JsonInput,
+  Textarea,
+  TextInput,
+} from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
 import { showNotification } from "@mantine/notifications";
-import { License, LicenseAccess } from "@prisma/client";
-import { ActionFunction, LoaderFunction } from "@remix-run/node";
+import type { License, LicenseAccess } from "@prisma/client";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { useState } from "react";
-import { Check, Key, Plus, World } from "tabler-icons-react";
+import { Check, Key, Plus } from "tabler-icons-react";
 import { getAccount } from "~/services/auth.server";
 import { db } from "~/services/db.server";
 import { hasPermission } from "~/services/permission.server";
 import generateToken from "~/services/token.server";
 
 interface LoaderData {
-    licenses: (License & {
-        accesses: LicenseAccess[];
-    })[]
+  licenses: (License & {
+    accesses: LicenseAccess[];
+  })[];
 }
 
 export default function LicenseOverview() {
-    const { licenses } = useLoaderData<LoaderData>();
+  const { licenses } = useLoaderData<LoaderData>();
 
-    const actionData = useActionData();
+  const actionData = useActionData();
 
-    return <Stack>
-        <Group>
-            <Title order={2}>Licenses</Title>
-            {JSON.stringify(actionData)}
-            <AddLicenseButton />
-        </Group>
-        <SimpleGrid cols={4}>
-            {licenses.map(license =>
-                <Card withBorder p="md" key={license.id}>
-                    <Group>
-                        <Key />
-                        {license.ipLimited ?
-                            <Badge>
-                                Ip Locked
-                            </Badge> : null}
-                    </Group>
-                    <Divider my="md" variant="dashed" />
-                    <Stack>
-                        <Text color="dimmed">Created at {new Date(license.createdAt).toDateString()}</Text>
-                    </Stack>
-                </Card>
-            )}
-        </SimpleGrid>
+  return (
+    <Stack>
+      <Group>
+        <Title order={2}>Licenses</Title>
+        {JSON.stringify(actionData)}
+        <AddLicenseButton />
+      </Group>
+      <SimpleGrid cols={4}>
+        {licenses.map((license) => (
+          <Card withBorder p="md" key={license.id}>
+            <Group>
+              <Key />
+              {license.ipLimited ? <Badge>Ip Locked</Badge> : null}
+            </Group>
+            <Divider my="md" variant="dashed" />
+            <Stack>
+              <Text color="dimmed">
+                Created at {new Date(license.createdAt).toDateString()}
+              </Text>
+            </Stack>
+          </Card>
+        ))}
+      </SimpleGrid>
 
-        {licenses.length === 0 ? <Text color="dimmed">No licenses found</Text> : null}
+      {licenses.length === 0 ? (
+        <Text color="dimmed">No licenses found</Text>
+      ) : null}
     </Stack>
+  );
 }
 
 const AddLicenseButton = () => {
-    const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
 
-    const [isTimeLimited, setIsTimeLimited] = useState<boolean>(false);
+  const [isTimeLimited, setIsTimeLimited] = useState<boolean>(false);
 
-    const [isIpLimited, setIsIpLimited] = useState<boolean>(false);
+  const [isIpLimited, setIsIpLimited] = useState<boolean>(false);
 
-    const onClose = () => {
-        setOpen(false);
-        setIsIpLimited(false);
-        setIsTimeLimited(false);
-    }
+  const onClose = () => {
+    setOpen(false);
+    setIsIpLimited(false);
+    setIsTimeLimited(false);
+  };
 
-    return <>
-        <Button leftIcon={<Plus />} onClick={() => setOpen(true)}>New</Button>
+  return (
+    <>
+      <Button leftIcon={<Plus />} onClick={() => setOpen(true)}>
+        New
+      </Button>
 
-        <Modal
-            opened={open}
-            centered
-            onClose={onClose}
-            title="Create new License"
-        >
-            <Form method="post">
-                <Stack>
-                    <TextInput label="Label" name="label" />
-                    <Switch value="timeLimited" label="Only valid until" checked={isTimeLimited} onChange={e => setIsTimeLimited(e.currentTarget.checked)} />
-                    <DatePicker name="validUntil" required={isTimeLimited} placeholder="Pick date" label="Valid until" disabled={!isTimeLimited}
-                        renderDay={(date) => {
-                            const day = date.getDate();
-                            return (
-                                <Indicator size={6} color="blue" offset={8} disabled={!isToday(date)}>
-                                    <div>{day}</div>
-                                </Indicator>
-                            );
-                        }} />
-                    <JsonInput name="payload" label="Payload" />
-                    <Switch name="ipLimited" value="ipLimited" label="Ip Limited" checked={isIpLimited} onChange={e => setIsIpLimited(e.currentTarget.checked)} />
-                    <Textarea name="ips" disabled={!isIpLimited} label="IP Whitelist" autosize placeholder="One IP mask per line" />
-                    <Button type="submit" onClick={() => {
-                        onClose();
-                        showNotification({
-                            title: "Created",
-                            message: "License has been created",
-                            icon: <Check />,
-                            color: "green",
-                        });
-                    }}>Create</Button>
-                </Stack>
-            </Form>
-
-        </Modal>
-    </>;
-}
+      <Modal
+        opened={open}
+        centered
+        onClose={onClose}
+        title="Create new License"
+      >
+        <Form method="post">
+          <Stack>
+            <TextInput label="Label" name="label" />
+            <Switch
+              value="timeLimited"
+              label="Only valid until"
+              checked={isTimeLimited}
+              onChange={(e) => setIsTimeLimited(e.currentTarget.checked)}
+            />
+            <DatePicker
+              name="validUntil"
+              required={isTimeLimited}
+              placeholder="Pick date"
+              label="Valid until"
+              disabled={!isTimeLimited}
+              renderDay={(date) => {
+                const day = date.getDate();
+                return (
+                  <Indicator
+                    size={6}
+                    color="blue"
+                    offset={8}
+                    disabled={!isToday(date)}
+                  >
+                    <div>{day}</div>
+                  </Indicator>
+                );
+              }}
+            />
+            <JsonInput name="payload" label="Payload" />
+            <Switch
+              name="ipLimited"
+              value="ipLimited"
+              label="Ip Limited"
+              checked={isIpLimited}
+              onChange={(e) => setIsIpLimited(e.currentTarget.checked)}
+            />
+            <Textarea
+              name="ips"
+              disabled={!isIpLimited}
+              label="IP Whitelist"
+              autosize
+              placeholder="One IP mask per line"
+            />
+            <Button
+              type="submit"
+              onClick={() => {
+                onClose();
+                showNotification({
+                  title: "Created",
+                  message: "License has been created",
+                  icon: <Check />,
+                  color: "green",
+                });
+              }}
+            >
+              Create
+            </Button>
+          </Stack>
+        </Form>
+      </Modal>
+    </>
+  );
+};
 
 const isToday = (someDate: Date) => {
-    const today = new Date()
-    return someDate.getDate() == today.getDate() &&
-        someDate.getMonth() == today.getMonth() &&
-        someDate.getFullYear() == today.getFullYear()
-}
+  const today = new Date();
+  return (
+    someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+  );
+};
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-    const account = await getAccount(request);
-    await hasPermission(params.appId!, account.id, true);
+  const account = await getAccount(request);
+  await hasPermission(params.appId!, account.id, true);
 
-    const licenses = await db.license.findMany({
-        include: {
-            accesses: {
-                orderBy: {
-                    time: "desc"
-                },
-                take: 1
-            }
+  const licenses = await db.license.findMany({
+    include: {
+      accesses: {
+        orderBy: {
+          time: "desc",
         },
-    });
+        take: 1,
+      },
+    },
+  });
 
-    return {
-        licenses
-    } as LoaderData
+  return {
+    licenses,
+  } as LoaderData;
 };
 
 export const action: ActionFunction = async ({ request, params }) => {
-    const account = await getAccount(request);
-    await hasPermission(params.appId!, account.id, true);
+  const account = await getAccount(request);
+  await hasPermission(params.appId!, account.id, true);
 
-    const formData = await request.formData();
+  const formData = await request.formData();
 
-    console.log("ipLimited", formData.has("ipLimited"));
+  console.log("ipLimited", formData.has("ipLimited"));
 
-    const license = await db.license.create({
-        data: {
-            key: generateToken(),
-            ipLimited: formData.has("ipLimited"),
-            ips: (formData.get("ips") as string).split("\n"),
-            payload: formData.get("payload") as string ?? "",
+  const license = await db.license.create({
+    data: {
+      key: generateToken(),
+      ipLimited: formData.has("ipLimited"),
+      ips: (formData.get("ips") as string)?.split("\n"),
+      payload: (formData.get("payload") as string) ?? "",
 
-            label: formData.get("label") as string ?? "",
+      label: (formData.get("label") as string) ?? "",
 
-            validUntil: formData.has("isTimeLimited") ? new Date(formData.get("validUntil") as string) : null,
+      validUntil: formData.has("isTimeLimited")
+        ? new Date(formData.get("validUntil") as string)
+        : null,
 
-            appId: params.appId!
-        }
-    });
+      appId: params.appId!,
+    },
+  });
 
-    return {
-        license
-    };
+  return {
+    license,
+  };
 };
